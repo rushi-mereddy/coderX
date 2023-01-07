0477c19c645bfa43e4d8205ae08409e9d375ef09
Rushi Mereddy <mereddyrushi@gmail.com>
2023-01-07 15:10:43 +0530

    changes

--- script.js
+++ script.js
@@ -25,3 +25,20 @@
 
             };
 
+
+//faq's
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("faq-active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
